c83b707abe6f501839806d4e75782fb0eaa297a0
William Boxhall <williamboxhall@gmail.com>
2014-06-02 19:14:48 +0200

    now keeping seqNo as long incase things get a little nuts. Twitter currently fits within a long

--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -9,33 +9,30 @@ import java.net.Socket;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 class Server {
 	public static void main(String args[]) {
 		try {
-			System.out.println("Running. Awaiting event source connection...");
+			System.out.print("Running. Awaiting event source connection... ");
 			Socket eventSource = new ServerSocket(9090).accept();
-
+			System.out.println("SUCCESS");
 
 			ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
 			serverSocketChannel.socket().bind(new InetSocketAddress(9099));
 			serverSocketChannel.configureBlocking(false);
 
 			Map<Integer, SocketChannel> clients = new HashMap<>();
-			Map<Integer, String> eventQueue = new HashMap<>();
+			Map<Long, String> eventQueue = new HashMap<>();
 
-			int lastDispatchedSeqNo = 0;
+			long lastDispatchedSeqNo = 0;
 
 			// for each batch
 			while (true) {
-				SocketChannel clientSocketChannel = serverSocketChannel.accept();
-				if (clientSocketChannel != null) {
-					acceptNewClients(clientSocketChannel, clients);
-				}
-				enqueueEvents(eventSource, eventQueue);
-				lastDispatchedSeqNo = processPrivateMessageEvents(eventQueue, clients, lastDispatchedSeqNo);
+				acceptNewClients(serverSocketChannel, clients);
+				// TODO what about disconnecting clients?
+				enqueueNextBatchOfEvents(eventSource, eventQueue);
+				lastDispatchedSeqNo = drainQueuedEventsInOrder(eventQueue, clients, lastDispatchedSeqNo);
 			}
 			// TODO close all the sockets
 		} catch (Exception e) {
@@ -44,37 +41,46 @@ class Server {
 		}
 	}
 
-	private static void acceptNewClients(SocketChannel clientSocketChannel, Map<Integer, SocketChannel> clients) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(clientSocketChannel.socket().getInputStream()));
-		int userId = Integer.parseInt(in.readLine());
-		System.out.println("User:" + userId);
-		clients.put(userId, clientSocketChannel);
-		in.close();
-	}
-
-	private static void enqueueEvents(Socket eventSource, Map<Integer, String> eventQueue) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(eventSource.getInputStream()));
-		while (in.ready()) {
-			String event = in.readLine();
-			System.out.println("Event:" + event);
-			int seqNo = Integer.parseInt(event.split("\\|")[0]);
-			eventQueue.put(seqNo, event);
+	private static void acceptNewClients(ServerSocketChannel serverSocketChannel, Map<Integer, SocketChannel> clients) throws IOException {
+		SocketChannel clientSocketChannel = serverSocketChannel.accept();
+		if (clientSocketChannel != null) {
+			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocketChannel.socket().getInputStream()));
+			int userId = Integer.parseInt(in.readLine());
+			System.out.println("User:" + userId);
+			clients.put(userId, clientSocketChannel);
+			in.close();
 		}
 	}
 
-	// TODO currently doesn't work for out of order temporal failures (follow event came after message)
-	private static int processPrivateMessageEvents(Map<Integer, String> eventQueue, Map<Integer, SocketChannel> clients, int lastDispatchedSeqNo) throws IOException {
-		while (!eventQueue.isEmpty()) {
-			lastDispatchedSeqNo = lastDispatchedSeqNo + 1;
-			String event = eventQueue.get(lastDispatchedSeqNo);
-			if (event == null) {
-				System.out.println("no event found for index " + lastDispatchedSeqNo);
+	private static void enqueueNextBatchOfEvents(Socket eventSource, Map<Long, String> eventQueue) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(eventSource.getInputStream()));
+		if (in.ready()) {
+			System.out.print("Events:");
+			while (in.ready()) {
+				String event = in.readLine();
+				long seqNo = Long.parseLong(event.split("\\|")[0]);
+				System.out.print(event + ",");
+				eventQueue.put(seqNo, event);
 			}
-			System.out.print(lastDispatchedSeqNo + ",");
-			eventQueue.remove(lastDispatchedSeqNo);
+			System.out.println();
+		}
+	}
+
+	private static long drainQueuedEventsInOrder(Map<Long, String> eventQueue, Map<Integer, SocketChannel> clients, long lastDispatchedSeqNo) throws IOException {
+		if (!eventQueue.isEmpty()) {
+			System.out.print("Dispatching:");
+			while (!eventQueue.isEmpty()) {
+				lastDispatchedSeqNo = lastDispatchedSeqNo + 1;
+				String event = eventQueue.get(lastDispatchedSeqNo);
+				if (event == null) {
+					throw new Error("No event found for " + lastDispatchedSeqNo);
+				}
+				System.out.print(lastDispatchedSeqNo + ",");
+				eventQueue.remove(lastDispatchedSeqNo);
+			}
+			System.out.println();
 		}
 		return lastDispatchedSeqNo;
-
 
 //		String event = eventQueue.poll();
 //		while (event != null) {
