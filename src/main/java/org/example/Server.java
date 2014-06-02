70a089a737e731fc0aa168643ef906a3369b0199
William Boxhall <williamboxhall@gmail.com>
2014-06-02 16:20:48 +0200

    make client socket accept not blocking

--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -3,12 +3,17 @@ package org.example;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 
 class Server {
 	private static final String DATA = "foo bar baz";
@@ -17,32 +22,42 @@ class Server {
 		try {
 			System.out.println("Running. Awaiting event source and client connection...");
 			Socket eventSource = new ServerSocket(9090).accept();
-			ServerSocket clientSocket = new ServerSocket(9099);
-			Map<Integer, Socket> clients = new HashMap<>();
-			List<String> eventQueue = new ArrayList<>();
-			do {
-				acceptNewClients(clientSocket, clients);
-			} while (true);
-			// TODO close all the sockets
 
+
+			ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+			serverSocketChannel.socket().bind(new InetSocketAddress(9099));
+			serverSocketChannel.configureBlocking(false);
+
+			Map<Integer, Socket> clients = new HashMap<>();
+			Queue<String> eventQueue = new LinkedList<>();
+
+			while (true) {
+				SocketChannel clientSocketChannel =
+						serverSocketChannel.accept();
+				if (clientSocketChannel != null) {
+					acceptNewClients(clientSocketChannel.socket(), clients);
+				}
+				//enqueueEvents(eventSource, eventQueue);
+			}
+			// TODO close all the sockets
 		} catch (Exception e) {
 			System.out.print("Whoops! It didn't work!\n");
 			e.printStackTrace();
 		}
 	}
 
-	private static void acceptNewClients(ServerSocket clientSocket, Map<Integer, Socket> clients) throws IOException {
-
-		Socket client = clientSocket.accept();
-		BufferedReader in = new BufferedReader(new
-				InputStreamReader(client.getInputStream()));
-		System.out.print("Stored client: ");
-
-		while (!in.ready()) {
-		}
+	private static void acceptNewClients(Socket client, Map<Integer, Socket> clients) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
 		int userId = Integer.parseInt(in.readLine());
-		System.out.println(userId);
+		System.out.println("User:" + userId);
 		clients.put(userId, client);
 		in.close();
+	}
+
+	private static void enqueueEvents(Socket eventSource, Queue<String> eventQueue) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(eventSource.getInputStream()));
+		String event = in.readLine();
+		System.out.println("Event:" + event);
+		eventQueue.add(event);
 	}
 }
