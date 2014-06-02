abc5664080521228c8032390db3e241e9c6aacbc
William Boxhall <williamboxhall@gmail.com>
2014-06-02 18:16:39 +0200

    no dispatching events in order using hashmap. Performs well because of fast hash lookups

--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -6,13 +6,11 @@ import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.Iterator;
 import java.util.Map;
-import java.util.Queue;
 
 class Server {
 	public static void main(String args[]) {
@@ -26,17 +24,18 @@ class Server {
 			serverSocketChannel.configureBlocking(false);
 
 			Map<Integer, SocketChannel> clients = new HashMap<>();
-			Queue<String> eventQueue = new LinkedList<>();
+			Map<Integer, String> eventQueue = new HashMap<>();
 
+			int lastDispatchedSeqNo = 0;
+
+			// for each batch
 			while (true) {
-				SocketChannel clientSocketChannel =
-						serverSocketChannel.accept();
+				SocketChannel clientSocketChannel = serverSocketChannel.accept();
 				if (clientSocketChannel != null) {
 					acceptNewClients(clientSocketChannel, clients);
 				}
 				enqueueEvents(eventSource, eventQueue);
-				processPrivateMessageEvents(eventQueue, clients);
-
+				lastDispatchedSeqNo = processPrivateMessageEvents(eventQueue, clients, lastDispatchedSeqNo);
 			}
 			// TODO close all the sockets
 		} catch (Exception e) {
@@ -53,40 +52,53 @@ class Server {
 		in.close();
 	}
 
-	private static void enqueueEvents(Socket eventSource, Queue<String> eventQueue) throws IOException {
+	private static void enqueueEvents(Socket eventSource, Map<Integer, String> eventQueue) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(eventSource.getInputStream()));
 		while (in.ready()) {
 			String event = in.readLine();
 			System.out.println("Event:" + event);
-			eventQueue.add(event);
+			int seqNo = Integer.parseInt(event.split("\\|")[0]);
+			eventQueue.put(seqNo, event);
 		}
 	}
 
 	// TODO currently doesn't work for out of order temporal failures (follow event came after message)
-	private static void processPrivateMessageEvents(Queue<String> eventQueue, Map<Integer, SocketChannel> clients) throws IOException {
-		String event = eventQueue.poll();
-		while (event != null) {
-			String[] parts = event.split("\\|");
-			String messageType = parts[1];
-
-
-			if (false && "P".equals(messageType)) {
-				int toUserId = Integer.parseInt(parts[4]);
-				String messagePayload = parts[0];
-
-				// TODO may fail
-				SocketChannel client = clients.get(toUserId);
-
-				ByteBuffer buf = ByteBuffer.allocate(48);
-				buf.clear();
-				buf.put(event.getBytes());
-
-				System.out.println(String.format("Sending user %d private message %s", toUserId, messagePayload));
-				client.write(buf);
+	private static int processPrivateMessageEvents(Map<Integer, String> eventQueue, Map<Integer, SocketChannel> clients, int lastDispatchedSeqNo) throws IOException {
+		while (!eventQueue.isEmpty()) {
+			lastDispatchedSeqNo = lastDispatchedSeqNo + 1;
+			String event = eventQueue.get(lastDispatchedSeqNo);
+			if (event == null) {
+				System.out.println("no event found for index " + lastDispatchedSeqNo);
 			}
-
-			event = eventQueue.poll();
+			System.out.print(lastDispatchedSeqNo + ",");
+			eventQueue.remove(lastDispatchedSeqNo);
 		}
+		return lastDispatchedSeqNo;
+
+
+//		String event = eventQueue.poll();
+//		while (event != null) {
+//			String[] parts = event.split("\\|");
+//			String messageType = parts[1];
+//
+//
+//			if (false && "P".equals(messageType)) {
+//				int toUserId = Integer.parseInt(parts[4]);
+//				String messagePayload = parts[0];
+//
+//				// TODO may fail
+//				SocketChannel client = clients.get(toUserId);
+//
+//				ByteBuffer buf = ByteBuffer.allocate(48);
+//				buf.clear();
+//				buf.put(event.getBytes());
+//
+//				System.out.println(String.format("Sending user %d private message %s", toUserId, messagePayload));
+//				client.write(buf);
+//			}
+//
+//			event = eventQueue.poll();
+//		}
 
 	}
 }
