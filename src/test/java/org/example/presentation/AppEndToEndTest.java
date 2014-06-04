bf585fb5625e577783834ef1ead0f818a9a8c5de
William Boxhall <williamboxhall@gmail.com>
2014-06-04 21:10:46 +0200

    Make AppEndToEndTest deterministic in the face of unpredictable thread behaviour

--- src/test/java/org/example/presentation/AppEndToEndTest.java
+++ src/test/java/org/example/presentation/AppEndToEndTest.java
@@ -4,7 +4,7 @@ import static java.lang.String.valueOf;
 import static org.example.presentation.App.CLIENT_PORT;
 import static org.example.presentation.App.EVENT_SOURCE_PORT;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.containsString;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -19,6 +19,7 @@ import org.junit.Test;
 public class AppEndToEndTest {
 	private static Socket eventSource;
 	private static Socket client;
+	private static int sequenceNumber = 1;
 
 	@BeforeClass
 	public static void startServer() throws IOException {
@@ -26,53 +27,47 @@ public class AppEndToEndTest {
 		client = new Socket("localhost", CLIENT_PORT);
 		eventSource = new Socket("localhost", EVENT_SOURCE_PORT);
 		write(client, "1");
+		waitForClientToFinishConnection(1);
 	}
 
 	@AfterClass
 	public static void stopServer() throws IOException {
 		eventSource.close();
 		client.close();
+		// App sockets closed by shutdown hook
 	}
 
 	@Test
-	public void allScenarios() throws IOException, InterruptedException {
-		assertClientReceiverRelevantEvents();
-		assertClientsMayDisconnectAndReconnect();
-	}
+	public void clientReceivesOnlyRelevantEvents() throws IOException, InterruptedException {
+		writeEvent("F|2|1");
+		writeEvent("F|3|4");
+		writeEvent("F|1|2");
+		writeEvent("F|3|1");
 
-	private void assertClientReceiverRelevantEvents() throws IOException {
-		write(eventSource, "1|F|2|1");
-		write(eventSource, "2|F|3|4");
-		write(eventSource, "3|F|1|2");
-		write(eventSource, "4|F|3|1");
-
-		assertThat(readLine(client), is("1|F|2|1"));
-		assertThat(readLine(client), is("4|F|3|1"));
-	}
-
-	private void assertClientsMayDisconnectAndReconnect() throws IOException, InterruptedException {
-		write(eventSource, "5|B");
-		assertThat(readLine(client), is("5|B"));
-
-		client.close();
-		write(eventSource, "6|B");
-		client = new Socket("localhost", CLIENT_PORT);
-		write(client, "1");
-
-		write(eventSource, "7|F|3|4");
-		write(eventSource, "8|U|3|4");
-		write(eventSource, "9|B");
-		assertThat(readLine(client), is("9|B"));
-	}
-
-	private static void write(Socket socket, String message) throws IOException {
-		new PrintWriter(socket.getOutputStream(), true).println(message);
+		assertThat(readLine(client), containsString("F|2|1"));
+		assertThat(readLine(client), containsString("F|3|1"));
 	}
 
 	private static String readLine(Socket socket) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		while (!reader.ready()) {
-		}
 		return reader.readLine();
+	}
+
+	private static void waitForClientToFinishConnection(int id) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+		while (!reader.ready()) {
+			writeEvent("P|99|" + id);
+		}
+		while (reader.ready()) {
+			assertThat(reader.readLine(), containsString("P|99|" + id));
+		}
+	}
+
+	private static void writeEvent(String eventSubstring) throws IOException {
+		write(eventSource, valueOf(sequenceNumber++) + "|" + eventSubstring);
+	}
+
+	private static void write(Socket socket, String message) throws IOException {
+		new PrintWriter(socket.getOutputStream(), true).println(message);
 	}
 }
